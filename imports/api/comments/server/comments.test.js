bdb5f363b92d60550603b618637c4f2ef58e5d74
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-07-19 07:24:45 +0300

    imports/api/comments/server/comments.test.js Add tests for the method comment.remove, a method that deletes a comment given its id

--- imports/api/comments/server/comments.test.js
+++ imports/api/comments/server/comments.test.js
@@ -9,31 +9,43 @@ import {Posts} from '/db';
 if (Meteor.isServer){
     describe('comments', function () {
         const commentOne = {
+            _id: 'commentid1',
             text: 'ONE I like this post',
             userId: 'test_user_id_1',
             postId: 'postid1'
         };
         const commentTwo = {
+            _id: 'commentid2',
             text: 'TWO I like this post',
             userId: 'test_user_id_2',
             postId: 'postid2'
         };
         const commentThree = {
+            _id: 'commentid3',
             text: 'THREE I like this post',
             userId: 'test_user_id_3',
             postId: 'postid1'
         };
-        const post = {
+        const postOne = {
             _id: 'postid1',
+            userId: 'post_user_id_1',
             title: 'This is about pictures',
             description: 'I like pictures a lot',
+            postType: 'nature'
+        };
+        const postTwo = {
+            _id: 'postid2',
+            userId: 'post_user_id_2',
+            title: 'This is about music',
+            description: 'I like music a lot',
             postType: 'nature'
         };
     
         beforeEach(function() {
             Comments.remove({});
             Posts.remove({});
-            Posts.insert(post);
+            Posts.insert(postOne);
+            Posts.insert(postTwo);
             Comments.insert(commentOne);
             Comments.insert(commentTwo);
             Comments.insert(commentThree);
@@ -66,14 +78,49 @@ if (Meteor.isServer){
 
         it('should give all comments that belong to a post', function () {
             let comments = Meteor.server.method_handlers['comment.list']
-                .apply({},[post._id]);
+                .apply({},[postOne._id]);
             comments = comments.filter((value, index, array) => {
-                return value.postId === post._id;
+                return value.postId === postOne._id;
             });
             assert.strictEqual(comments.length, 2);
         });
 
+        it('should remove a comment given the id if the authenticated user is the owner', function () {
+            Meteor.server.method_handlers['comment.remove']
+                .apply({userId: commentOne.userId}, [commentOne._id]);
+            assert.strictEqual(Comments.findOne({_id: commentOne._id}), undefined);
+        });
+
+        it('should not remove a comment given the id if the authenticated user is not the owner',
+            function () {
+                Meteor.server.method_handlers['comment.remove']
+                    .apply({userId: commentTwo.userId}, [commentOne._id]);
+                assert.notStrictEqual(Comments.findOne({_id: commentOne._id}), undefined);
+            }
+        );
+
+        it('should remove a comment given the id if the authenticated user is the owner of the post', 
+            function () {
+                Meteor.server.method_handlers['comment.remove']
+                    .apply({userId: postOne.userId}, [commentThree._id]);
+                assert.strictEqual(Comments.findOne({_id: commentThree._id}), undefined);                    
+            }
+        );
+
+        it('should not remove a comment given the id if the authenticated user is not the owner of the post', 
+            function () {
+                Meteor.server.method_handlers['comment.remove']
+                    .apply({userId: postOne.userId}, [commentTwo._id]);
+                assert.notStrictEqual(Comments.findOne({_id: commentTwo._id}), undefined);                    
+            }
+        );
+
         
+
+    
+
+
+
 
     });
 }
