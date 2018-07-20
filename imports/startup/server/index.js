a8b6c967e29615e2bd2c8a5dede8118b29ac6300
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-07-20 21:03:38 +0300

    imports/startup/server/index.js Import the file with secured methods for comments

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,6 @@
 import '/imports/api/methods'
 import '/imports/api/posts/methods';
 import '/imports/api/posts/securedMethods';
+import '/imports/api/comments/securedMethods';
 import '/imports/api/posts/publications';
 import '/imports/api/users/methods';
