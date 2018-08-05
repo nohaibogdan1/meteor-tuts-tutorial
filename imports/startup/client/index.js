0bb43850afa0513588a32bde68b3c41ec0d8cb11
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-08-05 18:41:42 +0300

    imports/startup/client/index.js , imports/startup/server/index.js Import db and exposures

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,1 +1,2 @@
 import '/imports/ui/init';
+import '/db/index';
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -3,4 +3,7 @@ import '/imports/api/posts/methods';
 import '/imports/api/posts/securedMethods';
 import '/imports/api/comments/securedMethods';
 import '/imports/api/posts/publications';
+import '/imports/api/comments/publications';
 import '/imports/api/users/methods';
+import '/db/index.js';
+import '/imports/api/exposures';
