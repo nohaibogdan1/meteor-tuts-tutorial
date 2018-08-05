bf5b188fde0210f5775ce2f83e69dbb30fd393c6
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-08-05 18:27:21 +0300

    db/index.js Import all links

--- db/index.js
+++ db/index.js
@@ -2,6 +2,10 @@ import Posts from './posts/collection';
 import Users from './users/collection';
 import Comments from './comments/collection';
 
+import './comments/links';
+import './posts/links';
+import './users/links';
+
 export {
     Posts,
     Users,
