61e936a779841d20513e6e2e1f554cdda73fa85b
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-08-06 11:48:13 +0300

    imports/ui/pages/Posts/PostList.jsx Add delete button and its functionality for each post

--- imports/ui/pages/Posts/PostList.jsx
+++ imports/ui/pages/Posts/PostList.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 export default class PostList extends React.Component {
     constructor() {
         super();
-        this.state = {posts: null};
+        this.state = {
+            posts: null
+        };
     }
 
     componentDidMount() {
-        Meteor.call('post.list', (err, posts) => {
+        Meteor.call('secured.post_list', (err, posts) => {
             this.setState({posts});
         });
+    }
+
+    delete(_id) {
+        Meteor.call('secured.post_remove', _id);
     }
 
     render() {
@@ -26,13 +32,22 @@ export default class PostList extends React.Component {
                     posts.map((post) => {
                         return (
                             <div key={post._id}>
-                                <p>Post id: {post._id} </p>
-                                <p>Post title: {post.title}, Post Description: {post.description} </p>
-                                <button onClick={() => {
-                                    history.push("/posts/edit/" + post._id)
-                                }}> Edit post
-                                </button>
+                                <p>Post title: {post.title}</p>
+                                <p>Post Description: {post.description}</p>
+                                <p>{(post.comments)?post.comments.length:'0'} comments, {post.views} views</p>
                                 <button onClick={() => {history.push("/posts/view/" + post._id)}}>See post</button>
+                                {(post.userId === Meteor.userId()) ? 
+                                    (<div>
+                                        <button onClick={() => {
+                                                history.push("/posts/edit/" + post._id)}
+                                            }> Edit post
+                                        </button>
+                                        <button onClick={() => {
+                                                this.delete(post._id)}
+                                            }>Delete post
+                                        </button>
+                                    </div>):undefined
+                                }
                             </div>
                         )
                     })}
