9bb1693aecfbe8acfe74a00d82857642d1c1e31d
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-08-09 20:48:02 +0300

    imports/api/comments/server/securedMethods.js Remove commentlist function

--- imports/api/comments/server/securedMethods.js
+++ imports/api/comments/server/securedMethods.js
@@ -1,13 +1,9 @@
 import {Meteor} from 'meteor/meteor';
-import CommentService from '/imports/api/comments/services';
+import {CommentService} from '/imports/api/comments/services/';
 
 Meteor.methods({
     'secured.comment_create' (comment) {
         return CommentService.createComment(comment);
-    },
-
-    'secured.comment_list' (postId) {        
-        return CommentService.listComments(postId);
     },
 
     'secured.comment_remove' (_id) {
