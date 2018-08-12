cd24b6b946ae277b909d6ca7733d1f726e6fb3ef
Nohai Bogdan <nohai.ionut02@gmail.com>
2018-08-12 12:47:14 +0300

    Add react component PostElement to show post items on  post list pages

---
+++ imports/ui/pages/Posts/PostElement.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+import PropTypes from 'prop-types';
+
+export default class PostElement extends React.Component {
+    constructor(props) {
+        super(props);
+        this.navigateToViewPage = this.navigateToViewPage.bind(this);
+        this.navigateToEditPage = this.navigateToEditPage.bind(this);
+        this.delete = this.delete.bind(this);
+
+        
+    }
+
+    navigateToViewPage() {
+        const {post} = this.props;
+        const {history} = this.props;
+        history.push("/posts/view/" + post._id);
+    }
+
+    navigateToEditPage() {
+        const {post} = this.props;
+        const {history} = this.props;
+        history.push("/posts/edit/" + post._id);
+    }
+
+    delete() {
+        const {post} = this.props;
+        Meteor.call('secured.post_remove', post._id);
+    }
+
+
+
+    render() {
+        const {post} = this.props;
+        return(
+            <div>
+                <p>Post title: {post.title}</p>
+                <p>Post Description: {post.description}</p>
+                <p>#{post.postType}</p>
+                <p>{(post.comments)?post.comments.length:'0'} comments, {post.views} views</p>
+                <button onClick={this.navigateToViewPage}>See post</button>
+                {(post.users && post.users._id === Meteor.userId()) ?
+                    (<div>
+                        <button onClick={this.navigateToEditPage}> Edit post</button>
+                        <button onClick={this.delete}>Delete post</button>
+                    </div>):undefined
+                }
+            </div>
+        );
+    }
+}
+
+PostElement.propTypes = {
+    post: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+}
