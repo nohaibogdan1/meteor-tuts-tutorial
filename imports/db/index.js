67d307bb710b3437de745f2b59b5a35b6ce5421f
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-10 18:18:51 +0300

    Import links in startup/server

--- imports/db/index.js
+++ imports/db/index.js
@@ -1,7 +1,6 @@
 import Posts from '/imports/db/posts/collection';
 import Users from '/imports/db/users/collection';
 import Comments from '/imports/db/comments/collection';
-import '/imports/db/links';
 
 export {
     Posts,
