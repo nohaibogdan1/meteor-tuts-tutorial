a4ab5c47874e5c76363115e57d87da65732d72c6
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-11 10:23:58 +0300

    Update method generateRoutes to use regex to replace all params in path

--- imports/ui/routes/methods.js
+++ imports/ui/routes/methods.js
@@ -1,35 +1,22 @@
 const generateRoutes = (path, params) => {
     // params is an object that consists of params for route 
-    // in order from first to last param
-
-    // firstly check if the number of params is equal with 
-    // the number of params necessary for the path
-    const numberOccurences = (path.match(/:/g)||[]).length;
-    var numberParams = Object.keys(params).length;
-    
-    if (numberParams != numberOccurences) {
-        alert('not have all params given');
-        return;
-    }
-
-    for (var param in params) {
-        if (params.hasOwnProperty(param)) {
-            const indexStartParam = path.indexOf(param, 0) - 1;
-            if (indexStartParam == -2) {
-                alert(`there is no ${param} in path`);
-                return;
-            }
-            let pathTillEndParam = path.substring(0, indexStartParam);
-            pathTillEndParam = pathTillEndParam + params[param];
-            indexEndParam = path.indexOf('/', indexStartParam);
-            if (indexEndParam == -1) {
-                indexEndParam = path.length;
-            }
-            let pathAfterParam  = path.substring(indexEndParam);
-            path = pathTillEndParam + pathAfterParam;
+    const paramOccurances = path.match(/(:[A-z])\w+/g);
+    // check if all params that path needs are received
+    let paramsNeeded = [];
+    for (let i = 0; i < paramOccurances.length; i++) {
+        if (!(paramOccurances[i].substring(1) in params)) {
+            paramsNeeded.push(paramOccurances[i].substring(1));
         }
     }
-
+    if (paramsNeeded.length != 0) {
+        console.log(`these ${paramsNeeded} params are needed`);
+        return;
+    }
+    // replace corresponding params in path
+    for (let i = 0; i < paramOccurances.length; i++) {
+        path = path.replace(paramOccurances[i], params[paramOccurances[i].substring(1)]);
+    }
+    console.log(path);
 }
 
 export default generateRoutes;
