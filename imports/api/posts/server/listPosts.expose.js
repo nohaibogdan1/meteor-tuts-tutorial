6aeb999accd89a359df21519f391c239a5758851
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-12 15:41:56 +0300

    Add firewall to test if user is loggedin

--- imports/api/posts/server/listPosts.expose.js
+++ imports/api/posts/server/listPosts.expose.js
@@ -1,3 +1,9 @@
 import listPostsQuery from '/imports/api/posts/queries/listPosts';
 
-listPostsQuery.expose({});
+listPostsQuery.expose({
+    firewall(userId, params) {
+        if(!userId) {
+            params.visibleForEveryone = true;
+        }
+    }
+});
