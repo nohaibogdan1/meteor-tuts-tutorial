277a0914752d0a8ea2b972655cadad2bca6bf584
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-12 19:55:59 +0300

    Revert "Add collection for reactions"
    This reverts commit f949f1db9d5f5ff776df9978a98c42d6e5f0d0c9.

--- imports/db/reactions/collection.js
+++
@@ -1,7 +0,0 @@
-import {Mongo} from 'meteor/mongo';
-
-import ReactionSchema from './schema';
-
-const Reactions = new Mongo.Collection('reactions');
-Reactions.attachSchema(ReactionSchema);
-export default Reactions;
