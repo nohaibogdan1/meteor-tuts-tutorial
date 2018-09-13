b351845e60599d51c06871169c6677fbf2b9205f
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-13 11:47:50 +0300

    Use PostService methods instead of calling meteor methods

--- imports/api/reactions/reactionService.js
+++ imports/api/reactions/reactionService.js
@@ -3,19 +3,19 @@ import {Meteor} from 'meteor/meteor';
 import Security from '/imports/api/security';
 import {Reactions} from '/imports/db';
 import listReactionsQuery from '/imports/api/reactions/queries/listReactions';
+import PostService from '/imports/api/posts/postService';
 
 export default class ReactionService {
     static updateReaction(reaction) {
         Security.checkLoggedIn(reaction.userId);
-        if (!Meteor.call('secured.post_get', reaction.postId)) {
+        if (!PostService.getPost(reaction.postId)) {
             throw new Meteor.Error('post does not exist');
         }
         Reactions.upsert({userId: reaction.userId, postId: reaction.postId}, { $set: {text: reaction.text}});
     };
 
     static listReactions(postId) {
-        const reactions = listReactionsQuery.clone({postId}).fetch();
-        return reactions;
+        return listReactionsQuery.clone({postId}).fetch();
     };
 
     static getReactionCurrentUser({postId, userId}) {
