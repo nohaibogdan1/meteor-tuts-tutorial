fc2622da7d8b7a79f245953efd7bbd704ae36fe5
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-13 09:17:25 +0300

    Add to reactionService listReactions and getReactionCurrentUser

--- imports/api/reactions/reactionService.js
+++ imports/api/reactions/reactionService.js
@@ -2,6 +2,7 @@ import {Meteor} from 'meteor/meteor';
 
 import Security from '/imports/api/security';
 import {Reactions} from '/imports/db';
+import listReactionsQuery from '/imports/api/reactions/queries/listReactions';
 
 export default class ReactionService {
     static updateReaction(reaction) {
@@ -10,5 +11,14 @@ export default class ReactionService {
             throw new Meteor.Error('post does not exist');
         }
         Reactions.upsert({userId: reaction.userId, postId: reaction.postId}, { $set: {text: reaction.text}});
-    }
+    };
+
+    static listReactions(postId) {
+        const reactions = listReactionsQuery.clone({postId}).fetch();
+        return reactions;
+    };
+
+    static getReactionCurrentUser({postId, userId}) {
+        return listReactionsQuery.clone({postId, userId}).fetchOne();
+    };
 }
