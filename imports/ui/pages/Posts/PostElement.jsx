ef76543427986da59abfa2e4b978824f691c9da8
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-13 09:20:38 +0300

    Get reactions number and reaction's text of current user

--- imports/ui/pages/Posts/PostElement.jsx
+++ imports/ui/pages/Posts/PostElement.jsx
@@ -17,9 +17,32 @@ export default class PostElement extends React.Component {
         this.navigateToEditPage = this.navigateToEditPage.bind(this);
         this.delete = this.delete.bind(this);
         this.addReaction = this.addReaction.bind(this);
+
+        this.state = {
+            reactionsNumber: null,
+            reactionCurrentUser: null
+        }
     }
 
+    componentDidMount(){
+        Meteor.call('secured.reactions_list', this.props.post._id, (error, reactions) => {
+            if (error) {
+                return console.log(error);
+            }
+            this.setState({reactionsNumber: reactions.length});
+            console.log('state.reactionsNumber', this.state.reactionsNumber);
+        });
 
+        Meteor.call('secured.reaction_get_current_user', {postId: this.props.post._id, userId: Meteor.userId()}, (error, reaction) => {
+            if (error) {
+                return console.log(error);
+            }
+            if (typeof reaction === 'undefined'){
+                return;
+            }
+            this.setState({reactionCurrentUser: reaction.text});
+        });
+    }
 
     navigateToViewPage() {
         const {post} = this.props;
@@ -38,15 +61,12 @@ export default class PostElement extends React.Component {
         Meteor.call('secured.post_remove', post._id);
     }
 
-    addReaction(a) {
-        console.log(a);
-        console.log(this);
+    addReaction(text) {
         reaction = {
             postId: this.props.post._id, 
-            text: 'sad', 
+            text, 
             userId: Meteor.userId()
         }
-
         Meteor.call('secured.reaction_update', reaction);
     }
 
@@ -65,12 +85,17 @@ export default class PostElement extends React.Component {
                         <button onClick={this.delete}>Delete post</button>
                     </div>):undefined
                 }
+
+                <p>reaction{this.state.reactionCurrentUser}</p>
+
                 <ReactionButton addReaction={this.addReaction} text={ReactionsEnum.LIKE}/>
                 <ReactionButton addReaction={this.addReaction} text={ReactionsEnum.LOVE}/>
                 <ReactionButton addReaction={this.addReaction} text={ReactionsEnum.HAPPY}/>
                 <ReactionButton addReaction={this.addReaction} text={ReactionsEnum.WOW}/>
                 <ReactionButton addReaction={this.addReaction} text={ReactionsEnum.SAD}/>
                 <ReactionButton addReaction={this.addReaction} text={ReactionsEnum.ANGRY}/>
+
+                
             </div>
         );
     }
