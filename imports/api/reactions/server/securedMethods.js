a2568822dcd5b28c1031cc9ba0bb356cf6306d8b
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-13 09:19:34 +0300

    Add to reaction securedMethods secured.reactions_list and secured.reaction_get_current_user

--- imports/api/reactions/server/securedMethods.js
+++ imports/api/reactions/server/securedMethods.js
@@ -5,5 +5,13 @@ import ReactionService from '../reactionService';
 Meteor.methods({
     'secured.reaction_update' (reaction) {
         ReactionService.updateReaction(reaction);
+    },
+
+    'secured.reactions_list' (postId) {
+        return ReactionService.listReactions(postId);
+    },
+
+    'secured.reaction_get_current_user' ({postId, userId}) {
+        return ReactionService.getReactionCurrentUser({postId, userId});
     }
 }); 
