7b4cffeffb82210d72442d32bcf3d3e7ba36e3f3
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-18 17:51:55 +0300

    It is not neccesary to save loadedPostsIds in state

--- imports/ui/pages/Posts/LoadMorePosts.jsx
+++ imports/ui/pages/Posts/LoadMorePosts.jsx
@@ -5,35 +5,26 @@ import listPostsQuery from '/imports/api/posts/queries/listPosts';
 export default class LoadMorePosts extends React.Component {
     constructor(props) {
         super(props);
-        const loadedPostsIds = props.posts.map((post) => {
-            return post._id;
-        });
-
-        this.state = {
-            loadedPostsIds
-        }   
     }
 
     loadPosts = () => {
-        const {limit} = this.props;
-        listPostsQuery.clone({_id: {"$nin": this.state.loadedPostsIds}, limit}).fetch((err, posts) => {
+        const {limit, posts} = this.props;
+        const postsIds = posts.map((post) => {
+            return post._id;
+        });
+        listPostsQuery.clone({_id: {"$nin": postsIds}, limit}).fetch((err, posts) => {
             if (err) {
                 return console.log(err);
             }
-            const loadedPostsIds = posts.map((post) => {
-                return post._id;
-            });
-            this.setState((prevState) => ({
-                loadedPostsIds: [...prevState.loadedPostsIds, ...loadedPostsIds]
-            }));
-            console.log('old posts: ', posts);
             this.props.getOldPosts(posts);
         });
     }
 
     render(){
         return (
-            <button onClick={this.loadPosts}>Load old posts</button>   
+            <div>
+                <button onClick={this.loadPosts}>Load old posts</button>   
+            </div>
         )
     }
 }
