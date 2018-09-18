e2b7a7c1190b6f7a345c5c1252b11e34a05d3e99
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-18 19:44:49 +0300

    Add showCategoryButons method that returns an array of CategoryButton components

--- imports/ui/pages/Posts/PostList.jsx
+++ imports/ui/pages/Posts/PostList.jsx
@@ -4,6 +4,8 @@ import listPostsQuery from '/imports/api/posts/queries/listPosts';
 import PostListDisplayContainer from './PostListDisplayContainer';
 import RoutesEnum from '/imports/ui/routes/enums/routes';
 import LoadMorePosts from './LoadMorePosts';
+import CategoryButton from './CategoryButton';
+import {types} from '/imports/db/posts/enums/types';
 
 export default class PostList extends React.Component { 
     constructor(props) {
@@ -11,7 +13,8 @@ export default class PostList extends React.Component {
         this.state = {
             loading: true,
             posts: [],
-            lastDate: null
+            lastDate: null,
+            category: null
         }
     }
 
@@ -29,7 +32,7 @@ export default class PostList extends React.Component {
 
     componentDidMount() {
         const limit = 2;
-        listPostsQuery.clone({limit}).fetch((err, posts) => {
+        listPostsQuery.clone({postType: this.state.category, limit}).fetch((err, posts) => {
             if (err) {
                 return console.log(err);
             }
@@ -48,6 +51,19 @@ export default class PostList extends React.Component {
 
     }
 
+    changeCategory = (category) => {
+        console.log('PostList category: ', category);
+        this.setState({category});
+    }
+
+    showCategoryButtons = () => {
+        return types.map((category) => {
+                return (
+                    <CategoryButton key={category} category={category} changeCategory={this.changeCategory}/>
+                )
+        });
+    }   
+
     render() {
         const {loading, posts, lastDate} = this.state;
         if (loading) {
@@ -55,6 +71,7 @@ export default class PostList extends React.Component {
         }
         return (
             <div>
+                {this.showCategoryButtons()}
                 <PostListDisplayContainer lastDate={lastDate} posts={posts} history={this.props.history} setPosts={this.setPosts}/>
                 <LoadMorePosts getOldPosts={this.getOldPosts} posts={posts} limit={2}/>
                 <button onClick={this.navigateToCreatePage}>Create a new post</button>
