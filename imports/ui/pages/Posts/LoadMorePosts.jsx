50fee62a5a9c8909822c7363b6058b6d337a078c
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-19 10:37:46 +0300

    Add postType to filter the posts

--- imports/ui/pages/Posts/LoadMorePosts.jsx
+++ imports/ui/pages/Posts/LoadMorePosts.jsx
@@ -4,11 +4,11 @@ import listPostsQuery from '/imports/api/posts/queries/listPosts';
 
 export default function LoadMorePosts(props) {
     loadPosts = () => {
-        const {limit, posts, getOldPosts} = props;
+        const {limit, posts, getOldPosts, postType} = props;
         const postsIds = posts.map((post) => {
             return post._id;
         });
-        listPostsQuery.clone({_id: {"$nin": postsIds}, limit}).fetch((err, posts) => {
+        listPostsQuery.clone({_id: {"$nin": postsIds}, limit, postType}).fetch((err, posts) => {
             if (err) {
                 return console.log(err);
             }
