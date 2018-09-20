b1450c685f64bb29e67a821937d4993ea8f7ee8d
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-09-20 15:01:36 +0300

    Add service facebook to ServiceConfiguration.configuration and add username and email to users

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,3 +1,27 @@
 import '/imports/db';
 import '/imports/db/links';
 import '/imports/api/server';
+
+import {ServiceConfiguration} from 'meteor/service-configuration';
+
+
+ServiceConfiguration.configurations.upsert(
+    { service: 'facebook' },
+    {
+        $set: {
+            appId: Meteor.settings.facebook.appId,
+            secret: Meteor.settings.facebook.secret
+        }
+    }
+);
+
+
+Accounts.onCreateUser((user) => {
+    if (!user.services.facebook) {
+        return user;
+    }
+    user.username = user.services.facebook.name;
+    user.emails = [{address: user.services.facebook.email}];
+    user.emails[0].verified = true;
+    return user;
+});
