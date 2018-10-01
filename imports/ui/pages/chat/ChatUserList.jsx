a09eff128eef0b56d72815bf01b2c2e438fa83b3
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 17:39:45 +0300

    Method listUsers renders ChatUser components for each user

---
+++ imports/ui/pages/chat/ChatUserList.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+
+import MessageListContainer from '../messages/MessageListContainer';
+import ChatUser from './ChatUser';
+
+export default class UserList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        Meteor.call('user.list', (err, users) => {
+            if (err) {
+                return console.log(err);
+            }
+            this.setState({users, loading: false});
+        });
+    }
+
+
+    sendUserId = (otherUserId) => {
+        this.props.getMessages(otherUserId);
+    }
+
+    listUsers = () => {
+        return (
+            this.state.users.map(user => {
+                return <ChatUser 
+                    key={user._id} 
+                    otherUserId={user._id}
+                    sendUserId={this.sendUserId}
+                    emailAddress={user.emailAddress}/>
+            })
+        )
+    }
+
+    render() {
+        const {loading} = this.state;
+        if (loading) {
+            return <div>Loading ...</div>
+        }
+        return (
+            <div>
+                {this.listUsers()}
+            </div>
+        )
+    }
+}
