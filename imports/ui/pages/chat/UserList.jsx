75f4306d3b56f5c5b56a303d2fdf9c84e3fb495f
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 15:28:10 +0300

    Add components UseList.jsx and MessageList.jsx

---
+++ imports/ui/pages/chat/UserList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class UserList extends React.Component {
+    render() {
+        return (
+            <div>
+                Left side - users list
+            </div>
+        )
+    }
+}
