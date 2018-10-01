722df8a2ba14f9eb0ad6e6f6e874849639e331cc
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 15:27:14 +0300

    Add style to ChatLayout component

--- imports/ui/pages/chat/ChatLayout.jsx
+++ imports/ui/pages/chat/ChatLayout.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
+import UserList from './UserList';
+import MessageList from '../messages/MessageList';
 
 export default ChatLayout = (props) => {
     return (
-        <div>
-            <div>
-                Left side - users list
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            margin: '0px auto',
+            width: '90rem'
+        }}>
+            <div style={{
+                backgroundColor: 'red',
+                width: '30rem',
+            }}>
+                <UserList/>
             </div>
-            <div>
-                Right side - messages list
+
+            <div style={{
+                backgroundColor: 'blue',
+                width:'70rem'
+            }}>
+                <MessageList/>
             </div>
         </div>
     )
