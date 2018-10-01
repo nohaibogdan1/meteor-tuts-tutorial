45fb32abd5f9daa24b2324745247b1e7bc176a06
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 16:16:31 +0300

    UserList class renders users list

--- imports/ui/pages/chat/UserList.jsx
+++ imports/ui/pages/chat/UserList.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import {Meteor} from 'meteor/meteor';
 
 export default class UserList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        Meteor.call('user.list', (err, users) => {
+            if (err) {
+                return console.log(err);
+            }
+            this.setState({users, loading: false});
+        });
+    }
+
+    listUsers = () => {
+        return (
+            this.state.users.map(user => {
+                return <div key={user._id}>{user._id}</div>
+            })
+        )
+    }
+
     render() {
+        const {loading} = this.state;
+        if (loading) {
+            return <div>Loading ...</div>
+        }
         return (
             <div>
-                Left side - users list
+                {this.listUsers()}
             </div>
         )
     }
