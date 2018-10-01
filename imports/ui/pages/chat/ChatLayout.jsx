b1aa0c57c0632daa9e20cce05db784510c296234
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 17:18:52 +0300

    ChatLayout sends as props to MessageListContainer the otherUserId  whom user communicates with

--- imports/ui/pages/chat/ChatLayout.jsx
+++ imports/ui/pages/chat/ChatLayout.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
 
 import UserList from './UserList';
-import MessageList from '../messages/MessageList';
+import MessageListContainer from '../messages/MessageListContainer';
 
-export default ChatLayout = (props) => {
-    return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-            margin: '0px auto',
-            width: '90rem'
-        }}>
-            <div style={{
-                backgroundColor: 'red',
-                width: '30rem',
-            }}>
-                <UserList/>
-            </div>
+export default class ChatLayout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            otherUserId: '0'
+        }
+    }
 
+    getMessages = (otherUserId) => {
+        console.log('getMessages', otherUserId);
+
+        this.setState({otherUserId});
+    }
+
+    render() {
+        const {otherUserId} = this.state;
+        return (
             <div style={{
-                backgroundColor: 'blue',
-                width:'70rem'
+                display: 'flex',
+                flexDirection: 'row',
+                margin: '0px auto',
+                width: '90rem'
             }}>
-                <MessageList/>
+                <div style={{
+                    backgroundColor: 'red',
+                    width: '30rem',
+                }}>
+                    <UserList getMessages={this.getMessages}/>
+                </div>
+
+                <div style={{
+                    backgroundColor: 'blue',
+                    width:'70rem'
+                }}>
+                    <MessageListContainer otherUserId={otherUserId}/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
