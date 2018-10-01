94c0372a3f79cce72bd6444fa8b5b961162e24d2
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 19:49:33 +0300

    Create MessageBox react component that creates a message

---
+++ imports/ui/pages/messages/MessageBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {AutoForm, AutoField, LongTextField, SelectField} from 'uniforms-unstyled';
+
+import FormSchema from './schema';
+
+export default MessageBox = (props) => {
+    const {otherUserId} = props;
+    submit = ({text}) => {
+        Meteor.call('secured.message_create',{text, otherUserId}, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+    return (
+        <AutoForm onSubmit={submit} schema={FormSchema}>
+            <AutoField name="text"/>
+            <button className="btn btn-outline-primary" type='submit'>Send</button>
+        </AutoForm>
+    )
+}
