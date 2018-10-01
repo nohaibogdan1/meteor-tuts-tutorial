f0bcd5e296568e1db52dad35fdc4d316a380f940
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 14:44:36 +0300

    Add react component ChatLayout

---
+++ imports/ui/pages/chat/ChatLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default ChatLayout = (props) => {
+    return (
+        <div>
+            <div>
+                Left side - users list
+            </div>
+            <div>
+                Right side - messages list
+            </div>
+        </div>
+    )
+}
