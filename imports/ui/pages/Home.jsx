4e7d2a33958f16795cfb925a16c1b12ab1c90d98
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-01 14:27:29 +0300

    Add button that navigates to chat page

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -3,6 +3,8 @@ import {Meteor} from 'meteor/meteor';
 import { AutoForm, AutoField } from 'uniforms-unstyled';
 import SimpleSchema from 'simpl-schema';
 
+import RoutesEnum from '/imports/ui/routes/enums/routes';
+
 export default class Home extends React.Component {
     constructor() {
         super();
@@ -10,13 +12,21 @@ export default class Home extends React.Component {
             number: -1
         }
     }
+
+    navigateToChat = () => {
+        const {history} = this.props;
+        history.push(RoutesEnum.CHAT);
+    }
+
     render() {
         const {number} = this.state;
         return (
             <div className="home">
                 Salut
-            </div>
-        )
+
+                <button onClick={this.navigateToChat}>Chat</button>
+            </div>   
+        );
     }
 };
 
