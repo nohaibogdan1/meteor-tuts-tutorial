996a488842651e176b05043ca0818555091b5369
nohaibogdan1 <nohai.ionut02@gmail.com>
2018-10-02 17:26:52 +0300

    Set params.userId at firewall of the listMessagesQuery

--- imports/api/messages/server/listMessages.expose.js
+++ imports/api/messages/server/listMessages.expose.js
@@ -4,6 +4,11 @@ listMessagesQuery.expose({
     firewall(userId, params) {
         if(!userId) {
             throw new Meteor.Error('not-allowed');
+        } 
+        if(params.otherUserId['$in'][1] === userId) {
+            params.userId = params.otherUserId;
+        } else {
+            throw new Meteor.Error('not-allowed');
         }
     }
 });
